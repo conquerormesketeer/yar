b9b001ddd2035eae2f9be8c2d53522087d3d3167
♗CσNϙUҽRσR_MҽSƙEƚҽҽR <79249487+conquerormesketeer@users.noreply.github.com>
2022-06-01 20:49:05 +0530

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -239,7 +239,11 @@ aiohttpsession = ClientSession()
 print("[INFO]: INITIALIZING ARQ CLIENT")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+ubot2 = 
+pbot = Client("ErenPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot.start()
+telethn = TelegramClient("luna", API_ID, API_HASH)
+
 try:
     ubot2.start()
 except BaseException:
